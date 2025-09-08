1bef90b48245f89f8f1a5a3bb29c26c587850237
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-08 20:06:57 -0300

    Realizando testes.

--- OO_Sabor_express/app.py
+++ OO_Sabor_express/app.py
@@ -3,12 +3,16 @@ from modelos.Cardapio.bebida import Bebida
 from modelos.Cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça','gourmet')
+
 bebida_suco = Bebida('Suco de Menancia',5.0,'grande') 
 prato_paozinho = Prato('Paozinho',2.00,'O melhor pao da cidade')
 
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+
 def main():
     #Restaurante.listar_restaurantes()
-    
+
     print(bebida_suco)
     print(prato_paozinho)
 
