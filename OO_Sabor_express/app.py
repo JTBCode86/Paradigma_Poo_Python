5bacb2aa89c5f9052b3ef75fb4c0a5f49570f4ef
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-08 20:23:27 -0300

    Implementando o metodo para adicionar ao cardapio.

--- OO_Sabor_express/app.py
+++ OO_Sabor_express/app.py
@@ -7,8 +7,8 @@ restaurante_praca = Restaurante('praça','gourmet')
 bebida_suco = Bebida('Suco de Menancia',5.0,'grande') 
 prato_paozinho = Prato('Paozinho',2.00,'O melhor pao da cidade')
 
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
     #Restaurante.listar_restaurantes()
