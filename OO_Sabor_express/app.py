562d14115219a2f0dc4ca8c68ae69982b3e782f9
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-09-09 19:27:08 -0300

    Ajustando o codigo.

--- OO_Sabor_express/app.py
+++ OO_Sabor_express/app.py
@@ -1,12 +1,12 @@
 from modelos.restaurante import Restaurante
-from modelos.Cardapio.bebida import Bebida
-from modelos.Cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
-restaurante_praca = Restaurante('praça','gourmet')
-
-bebida_suco = Bebida('Suco de Menancia',5.0,'grande') 
-prato_paozinho = Prato('Paozinho',2.00,'O melhor pao da cidade')
-
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
